18ee6d64b19cc4766cc3774dd9181cffa5ea243f
JoeAngel <joe10330@gmail.com>
2015-04-30 15:10:21 +0800

    Smaple for loading Youtube video

---
+++ README.md
@@ -0,0 +1,8 @@
+# Abount
+* base on project: [XCDYouTubeKut][https://github.com/0xced/XCDYouTubeKit]
+* using [CococoaPds](https://cocoapods.org/)
+* Version
+** XCDYouTubeKit 2.1.2
+** Xcode 6.3.1
+** Swift 1.2
+
--- Youtube/ViewController.swift
+++ Youtube/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    var videoItem: YoutubeVideo = YoutubeVideo();
+    
+/*
+    var detailItem: YoutubeVideo? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+*/
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //self.configureView()
+    }
+    
+    @IBAction func trigger() {
+        self.configureView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func configureView() {
+        println("configureView")
+        // Update the user interface for the detail item.
+        
+        if let label = self.detailDescriptionLabel {
+            label.text = videoItem.title as? String
+            self.title = videoItem.title as? String
+        }
+        let idVideo = videoItem.id as? String
+        var videoPlayerViewController: XCDYouTubeVideoPlayerViewController = XCDYouTubeVideoPlayerViewController(videoIdentifier: idVideo);
+        videoPlayerViewController.presentInView(self.view);
+        videoPlayerViewController.moviePlayer.controlStyle = MPMovieControlStyle.None
+        videoPlayerViewController.moviePlayer.play()
+        self.view.addSubview(videoPlayerViewController.view)
+    }
 
 }
 
